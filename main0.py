ba5843f0b92861ad3a59fce753aa6ba46a78c1b9
gharesh14 <135325288+gharesh14@users.noreply.github.com>
2024-10-05 06:22:54 +0530

    Add files via upload

--- main0.py
+++ main0.py
@@ -9,7 +9,6 @@ from transformers import pipeline
 from PIL import Image
 import requests
 from ultralytics import YOLO
-import time
 
 
 
@@ -34,7 +33,7 @@ def get_voice_input():
 #real time input in the form of image (open cv)
 def memory_recall(context):
 
-        
+        import time
 
         # Initialize the camera
         cap = cv2.VideoCapture(0)
@@ -117,36 +116,42 @@ def sense_res(image_path):
     return response.text
 
 def nav(image_path,text):
-   
+
     model = genai.GenerativeModel("gemini-1.5-flash")
-    dlmodel=YOLO('yolov8n_openvino_model')
-        # Predict with the model
-    results = dlmodel.predict(image_path, stream=True,conf=0.5)
+    ov_model=YOLO('yolov8n_openvino_model')
+    dlmodel=YOLO('best.pt')
+    # Predict with the model
+    results=dlmodel.predict(image_path,stream=True,conf=0.5)
     for i, r in enumerate(results):
         # Plot results image
         im_bgr = r.plot()  # BGR-order numpy array
         im_rgb = Image.fromarray(im_bgr[..., ::-1])  # RGB-order PIL image
+        # Save results to disk
+        r.save(filename=f"resultsseg.jpg")
 
-            # Show results to screen (in supported environments)
-        r.show()
+    results2= ov_model.predict(image_path, stream=True,conf=0.5)
+    for i, r in enumerate(results2):
+        # Plot results image
+        im_bgr = r.plot()  # BGR-order numpy array
+        im_rgb = Image.fromarray(im_bgr[..., ::-1])  # RGB-order PIL image
+        # Save results to disk
+        r.save(filename=f"resultsov.jpg")
+    pic = PIL.Image.open(image_path)
+    pic2=PIL.Image.open('./images/depth_image.png')
+    pic3=PIL.Image.open("resultsov.jpg")
+    pic4=PIL.Image.open('resultsseg.jpg')
 
-            # Save results to disk
-        r.save(filename=f"results.jpg")
-        pic = PIL.Image.open(image_path)
-        pic2=PIL.Image.open('./images/depth_image.png')
-        pic3=PIL.Image.open("results.jpg")
+    prompt= f"""You are a navigation guide capable of indoor navigation according to the user's desire : {text}, You've been provided with 4 images. each contains a parameter for your information
+     image1 is the actual unaltered image for human vision, Image2: It contains depth estimation with varying pixel density where intensity decrease with increase in distance
+      image3: It contains any obstacles which might be a hindrance for the user to navigate through so guide accordingly, Image4: Contains path and walls segmented and give me route if path is available 
+      INSTRUCTIONS:
+      GIVE me step by step direction
+      the number of steps needed to reach the destination"""
+    
+    response = model.generate_content([pic,pic2,pic3,pic4,prompt])
+    return response.text
 
-        prompt= f"""You are a visual navigation guide capable of giving step by step route as the user desires.User's desire: {text}
-        INSTRUCTIONS: You are provided with 3 images for the same environment the first pic is the actual environment in which you are supposed to show route for
-        the second image is the depth estimation from the user, the increase in pixel intensity represents closeness, the third image consists of plots made up by object detection model to aid you in clearly showing the route avoiding the objects through the available path.
-        GUIDANCE:
-        1.describe the user's destination and their path
-        2.provide details for navigating
-        3. Identify obstacles in the image which might interfere with the user's path
-        4.clearly specify the estimates amount of steps the user should take in order to reach the destination"""
-        
-        response = model.generate_content([pic,pic2,pic3,prompt])
-        return response.text
+
 
 
 def speak_text(text):
@@ -161,13 +166,13 @@ def voice_input():
 
     if user_input:
         if "describe" in user_input.lower():
-            image_path ="G:/INTEL HACKATHON/images/ven2.jpg"
+            image_path ="/images/npcroom.jpg"
             if image_path:
                 search_result = scene_desc(image_path)
                 speak_text(search_result)
                 print(search_result)
         elif "search" in user_input.lower():
-            image_path ="G:/INTEL HACKATHON/images/ven2.jpg"
+            image_path ="/images/npcroom.jpg"
 
             if image_path:
                 search_result = sense_res(image_path)
@@ -175,7 +180,7 @@ def voice_input():
                 print(search_result)
 
         elif "route" in user_input.lower():
-            image_path ="G:/INTEL HACKATHON/images/ven2.jpg"
+            image_path ="images/npcroom.jpg"
             if image_path:
                 search_result = nav(image_path,text)
                 speak_text(search_result)
