66711c732d27d7f880a6678befb83f062a275884
gharesh14 <135325288+gharesh14@users.noreply.github.com>
2024-10-05 08:29:46 +0530

    main0.py

--- main0.py
+++ main0.py
@@ -9,9 +9,12 @@ from transformers import pipeline
 from PIL import Image
 import requests
 from ultralytics import YOLO
+from transformers import DPTImageProcessor, DPTForDepthEstimation
+import torch
+import numpy as np
 
-
-
+image_processor = DPTImageProcessor.from_pretrained("Intel/dpt-hybrid-midas")
+DE_model = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas")
 #function for voice input
 def get_voice_input():
     recognizer = sr.Recognizer()
@@ -58,7 +61,7 @@ def memory_recall(context):
 
                     # Wait for 1 minute before capturing the next image
                     time.sleep(60)
-                    model=genai.GenerativeModel("gemini-1.5-pro")
+                    model=genai.GenerativeModel("gemini-1.5-flash")
                     
                     pic = PIL.Image.open(frame)
                     if not context:
@@ -117,9 +120,29 @@ def sense_res(image_path):
 
 def nav(image_path,text):
 
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-1.5-pro")
     ov_model=YOLO('yolov8n_openvino_model')
     dlmodel=YOLO('best.pt')
+    image=Image.open(image_path)
+    inputs = image_processor(images=image, return_tensors="pt")
+    # Perform inference
+    with torch.no_grad():
+        outputs = DE_model(**inputs)
+        predicted_depth = outputs.predicted_depth
+
+    # Interpolate to the original image size
+    prediction = torch.nn.functional.interpolate(
+        predicted_depth.unsqueeze(1),
+        size=image.size[::-1],
+        mode="bicubic",
+        align_corners=False,
+)
+    # Convert the prediction to a NumPy array and scale it
+    output = prediction.squeeze().cpu().numpy()
+    formatted = (output * 255 / np.max(output)).astype("uint8")
+    depth = Image.fromarray(formatted)
+    depth.save('depth.jpg') 
+
     # Predict with the model
     results=dlmodel.predict(image_path,stream=True,conf=0.5)
     for i, r in enumerate(results):
@@ -137,18 +160,23 @@ def nav(image_path,text):
         # Save results to disk
         r.save(filename=f"resultsov.jpg")
     pic = PIL.Image.open(image_path)
-    pic2=PIL.Image.open('./images/depth_image.png')
+    pic2=PIL.Image.open('depth.jpg')
     pic3=PIL.Image.open("resultsov.jpg")
     pic4=PIL.Image.open('resultsseg.jpg')
 
-    prompt= f"""You are a navigation guide capable of indoor navigation according to the user's desire : {text}, You've been provided with 4 images. each contains a parameter for your information
-     image1 is the actual unaltered image for human vision, Image2: It contains depth estimation with varying pixel density where intensity decrease with increase in distance
-      image3: It contains any obstacles which might be a hindrance for the user to navigate through so guide accordingly, Image4: Contains path and walls segmented and give me route if path is available 
-      INSTRUCTIONS:
-      GIVE me step by step direction
-      the number of steps needed to reach the destination"""
+    prompt= f"""You are a visual navigation guide capable of giving step by step route as my desire .My desired destination: {text}
+    INSTRUCTIONS: You are provided with 4 images for the same environment the first pic is the actual environment in which you are supposed to show route for
+    the second image is the depth estimation from the user, the increase in darkness represents decrease in depth, the third image consists of object detection to clearly identify the obstacles, and the fourth image does segmentation to show you the path which the user can follow if there is no path the user cannot travel to the desired location
+    reply with only text and NEVER use ''
+    GUIDANCE:
+    1.describe the user's destination and their path
+    2.provide details for navigating
+    3. Identify obstacles in the image and in how many would I reach the said obstacle which might interfere with my path
+    4.clearly specify the estimates amount of steps the user should take in order to reach the destination
+    """
     
     response = model.generate_content([pic,pic2,pic3,pic4,prompt])
+    print(response)
     return response.text
 
 
@@ -180,7 +208,7 @@ def voice_input():
                 print(search_result)
 
         elif "route" in user_input.lower():
-            image_path ="images/npcroom.jpg"
+            image_path ="images/ethnoverandah.jpg"
             if image_path:
                 search_result = nav(image_path,text)
                 speak_text(search_result)
